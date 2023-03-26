f8ec737ec66a62184ba81465887986159ca53f35
hyuk12 <hotman7025@naver.com>
2023-03-26 17:21:51 +0900

    회원 도메인 개발과 테스트

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     *  회원 가입
+     */
+
+    private void validateDuplicatedMember(Member member) {
+        // EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+
+    public Member findById(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given (주어지면)
+        Member member = new Member();
+        member.setName("홍길동");
+
+        // when (이것을 실행하면)
+        Long savedId = memberService.join(member);
+
+        // then ( 이렇게 결과가 나와야해)
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2); // 예외가 발생을 해야한다.
+
+        // then
+        fail("예외가 발생해야 한다.");
+    }
+
+}
