fdcc97b96167902a2ed866ee8c4964787b3411bd
hyuk12 <hotman7025@naver.com>
2023-03-27 17:49:46 +0900

    jpa 앞단 화면 view

---
+++ jpashop/src/main/java/jpabook/jpashop/web/ItemController.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+
+    }
+}
